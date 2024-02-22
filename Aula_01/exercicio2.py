70bcee30e8a086132b81b21b6b45a87e9a5e95e1
Mateus Vitor Cavalcanti Rodrigues <20232014040031@ifrn.local>
2024-02-22 17:28:15 -0300

    Exercicio2

---
+++ Aula_01/exercicio2.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome completo:")
+x = nome.split()
+print("Bem-vindo ao Python,", x[0])
--- Aula_01/olá_mundo.py
+++ Aula_01/olá_mundo.py
@@ -1,2 +1,3 @@
 print("Olá Mundo")
 print("Testando o Codespaces")
+print("Repositório atualizado")
