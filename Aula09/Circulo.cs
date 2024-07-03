f2846e1853eabcea78c9faf8b71c9bc5bf297382
Mateus Vitor <43605106+MaVitor@users.noreply.github.com>
2024-07-03 17:20:35 +0000

    exercicio1c#

---
+++ Aula09/Circulo.cs
@@ -0,0 +1,9 @@
+class Circulo{
+    public double r;
+    public double CalcAreaC(){
+        return Math.PI * (r*r);
+    }
+    public double CalcCirc(){
+        return 2 * Math.PI * r;
+    }
+}
--- Aula09/Program.cs
+++ Aula09/Program.cs
@@ -1,8 +1,16 @@
-﻿Triangulo x = new Triangulo();                     // x = Triangulo()
-Console.WriteLine(x);                              // print(x)
-Console.WriteLine(x.CalcArea());                   // print(x.calc_area())
-Console.WriteLine("Informe o valor da base: ");
-x.b = double.Parse(Console.ReadLine());            // x.b = float(input())
-Console.WriteLine("Informe o valor da altura: ");
-x.h = double.Parse(Console.ReadLine());
-Console.WriteLine(x.CalcArea());
+﻿// Triangulo x = new Triangulo();                     // x = Triangulo()
+// Console.WriteLine(x);                              // print(x)
+// Console.WriteLine(x.CalcArea());                   // print(x.calc_area())
+// Console.WriteLine("Informe o valor da base: ");
+// x.b = double.Parse(Console.ReadLine());            // x.b = float(input())
+// Console.WriteLine("Informe o valor da altura: ");
+// x.a = double.Parse(Console.ReadLine());
+// Console.WriteLine(x.CalcArea());
+
+Circulo x = new Circulo();
+Console.WriteLine("Informe o valor do raio do circulo: ");
+x.r = double.Parse(Console.ReadLine());
+Console.WriteLine("Area: ");
+Console.WriteLine(x.CalcAreaC());
+Console.WriteLine("Circunferencia: ");
+Console.WriteLine(x.CalcCirc());
